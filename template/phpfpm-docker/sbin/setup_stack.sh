f9dc2f8dfed3a5c8f04619bd163cfe0f63c52285
Chan Chung <chung.chan@cs.cityu.edu.hk>
2023-07-09 07:33:22 +0800

    cronjob added

--- template/phpfpm-docker/sbin/setup_stack.sh
+++ template/phpfpm-docker/sbin/setup_stack.sh
@@ -5,3 +5,4 @@ set -eux
 php /var/www/html/admin/cli/cfg.php --component=qtype_stack --name=platform --set=server
 php /var/www/html/admin/cli/cfg.php --component=qtype_stack --name=maximaversion --set=5.41.0
 php /var/www/html/admin/cli/cfg.php --component=qtype_stack --name=maximacommand --set=maxima:8080/MaximaPool/MaximaPool
+php /var/www/html/admin/cli/cfg.php --component=qtype_stack --name=maximalibraries --set="stats, distrib, descriptive, simplex"
