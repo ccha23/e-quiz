8b9e4764ef9c41117caed6aca9a76a195b5b58db
Chung Chan <chung.chan@cityu.edu.hk>
2023-11-21 03:53:49 +0800

    refactor

--- template/html-docker/bin/get_moodle.sh
+++ template/html-docker/bin/get_moodle.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Download Moodle and extract it
-#  4.2+: https://download.moodle.org/download.php/stable402/moodle-latest-402.tgz \
+#  4.3+: https://download.moodle.org/download.php/stable403/moodle-latest-403.tgz
+#  4.3: https://download.moodle.org/download.php/stable403/moodle-4.3.tgz
+#     of https://github.com/moodle/moodle/archive/refs/tags/v4.3.0.tar.gz
+#  4.2+: https://download.moodle.org/download.php/stable402/moodle-latest-402.tgz
 #  4.2: https://download.moodle.org/download.php/stable402/moodle-4.2.tgz
 #    or https://github.com/moodle/moodle/archive/refs/tags/v4.2.0.tar.gz
 wget -O /tmp/moodle-4.2.tgz \
