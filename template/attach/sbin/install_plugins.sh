f7fa61a3661de0bb44c7c063f110a2eea2236167
Chan Chung <chung.chan@cs.cityu.edu.hk>
2024-07-16 18:48:16 +0800

    moosh upgrade fixed an issue

--- template/attach/sbin/install_plugins.sh
+++ template/attach/sbin/install_plugins.sh
@@ -3,7 +3,7 @@
 set -eux
 
 moosh plugin-list
-moosh plugin-install -fd editor_marklar && (cd /var/www/html/lib/editor && mv mudrd8mz-moodle-editor_marklar-98ed92e marklar)
+moosh plugin-install -fd editor_marklar
 moosh plugin-install -fd qtype_drawing
 moosh plugin-install -fd qtype_pmatch
 moosh plugin-install -fd qtype_ordering
