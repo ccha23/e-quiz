500262fffb446c499f81dc2a306dd8e96641f112
Zartist <shuyizhao060@gmail.com>
2026-05-19 15:17:51 +0800

    upgrade moodle to 5.2.0

--- template/html-docker/bin/get_moodle.sh
+++ template/html-docker/bin/get_moodle.sh
@@ -4,6 +4,6 @@
 # See https://github.com/moodle/moodle/tags
 #     https://download.moodle.org/releases/latest/
 wget -O /tmp/moodle.tgz \
-https://github.com/moodle/moodle/archive/refs/tags/v5.1.3.tar.gz \
+https://github.com/moodle/moodle/archive/refs/tags/v5.2.0.tar.gz \
 && tar -zxf /tmp/moodle.tgz -C /var/www/html --strip-components=1 \
 && rm /tmp/moodle.tgz
