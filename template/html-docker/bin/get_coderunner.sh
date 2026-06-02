f8f4270bf04891f3e6c4ade6cf6c46b3ed889c5a
SophieZHAO111 <shuyzhao8@cityu.edu.hk>
2026-06-02 18:46:46 +0800

    upgrade Coderunner and Adaptive adapted for coderunner to latest

--- template/html-docker/bin/get_coderunner.sh
+++ template/html-docker/bin/get_coderunner.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+!/bin/bash
 
 # Download CodeRunner question type and extract it
 # See https://github.com/trampgeek/moodle-qtype_coderunner/tags
-wget https://github.com/trampgeek/moodle-qtype_coderunner/archive/refs/tags/v5.8.0.tar.gz \
+wget https://github.com/trampgeek/moodle-qtype_coderunner/archive/refs/tags/v5.9.2.tar.gz \
     -O /tmp/coderunner.tgz \
 && mkdir -p /var/www/html/public/question/type/coderunner \
 && tar -zxf /tmp/coderunner.tgz \
@@ -11,7 +11,7 @@ wget https://github.com/trampgeek/moodle-qtype_coderunner/archive/refs/tags/v5.8
 
 # Download CodeRunner adative question behavior and extract it
 # See https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner/tags
-wget https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner/archive/refs/tags/v1.4.4.tar.gz \
+wget https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner/archive/refs/tags/v1.4.5.tar.gz \
     -O /tmp/adaptive_adapted_for_coderunner.tgz \
 && mkdir -p /var/www/html/public/question/behaviour/adaptive_adapted_for_coderunner \
 && tar -zxf /tmp/adaptive_adapted_for_coderunner.tgz \
