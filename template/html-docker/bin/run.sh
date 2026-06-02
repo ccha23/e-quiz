c2c8fe0f365612f08530edf98c5af48b0e8793e6
SophieZHAO111 <shuyzhao8@cityu.edu.hk>
2026-06-02 18:57:32 +0800

    Modify the comments in run.sh

--- template/html-docker/bin/run.sh
+++ template/html-docker/bin/run.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# ??????????(????????????,???????)
 if [ ! -f /output/.initialized ]; then
     echo "Initializing /var/www/html..."
     rsync -az --ignore-existing --chown=www-data:www-data  /var/www/html/. /output/ && echo "Done"
 fi
 
-# ================= ??:???????? =================
-echo "--> ????????????????..."
+echo "--> Checking and forcibly syncing the latest version of the plugin...."
 
-# ???????????,????
+# Ensure the target directory exists on the hard drive to prevent errors.
 mkdir -p /output/public/question/type/coderunner
 mkdir -p /output/public/question/behaviour/adaptive_adapted_for_coderunner
 
-# ??????????????????(??????? --ignore-existing,???? -c ??)
+# Force the new plugin code from the image to be overwritten on the hard drive.
 rsync -azc --chown=www-data:www-data /var/www/html/public/question/type/coderunner/ /output/public/question/type/coderunner/
 rsync -azc --chown=www-data:www-data /var/www/html/public/question/behaviour/adaptive_adapted_for_coderunner/ /output/public/question/behaviour/adaptive_adapted_for_coderunner/
 
-echo "--> ??????!"
-# ==========================================================
+echo "--> Plugin synchronization complete!"
